1d05ad8dd55b2971eac117b5f4e9147787575837
kikisam-22 <kiadysam@gmail.com>
2023-12-27 20:06:36 +0100

    Modified function "createDivs". Bug : only 32 divs instead of 252 divs created. Adding style to divs created and flex to div container.

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const gridContainer = document.querySelector("#grid-container");
 
 function createDivs(numberOfRows, numberOfColumns) {
+    numberOfColumns = 16;
+    numberOfRows = 16;
     for (numberOfRows; numberOfRows>0; numberOfRows--) {
         let gridNumberOfRows = document.createElement("div");
         gridNumberOfRows.classList.add("row");
-        gridNumberOfRows.setAttribute("id",`row_${numberOfRows}`);
+        gridNumberOfRows.setAttribute("id",`row-${numberOfRows}`);
+        gridNumberOfRows.style.cssText = "background-color:white; border:2px solid black; height:100px; width:100px";
         gridContainer.appendChild(gridNumberOfRows);
-    }
-
-    for (numberOfColumns; numberOfColumns>0; numberOfColumns--){
-        let gridNumberOfColumns = document.createElement("div");
-        gridNumberOfColumns.classList.add("row");
-        gridNumberOfColumns.setAttribute("id", `column_${numberOfColumns}`);
-        gridContainer.appendChild(gridNumberOfColumns);
+        for (numberOfColumns--; numberOfColumns>0; numberOfColumns--){
+            let gridNumberOfColumns = document.createElement("div");
+            gridNumberOfColumns.classList.add("column");
+            gridNumberOfColumns.setAttribute("id", `row-${numberOfRows}-column-${numberOfColumns}`);
+            gridNumberOfColumns.style.cssText = "background-color:white; border:2px solid black; height:100px; width:100px"
+            gridContainer.appendChild(gridNumberOfColumns);
+        }
+        numberOfColumns = 16;
     }
 }
 
-createDivs(16, 16)
+createDivs()
