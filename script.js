64a3c35f96276df22385c05ad8ddd0015ca2a536
kikisam-22 <kiadysam@gmail.com>
2023-12-27 23:30:35 +0100

    Adding Event Listener to grid. The event listener will add a class to the grid when mouse overs the grid. The class has a black style background.

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const gridContainer = document.querySelector("#grid-container");
+let gridNumberOfColumns;
+let gridNumberOfRows;
 
 function createDivs(numberOfRows, numberOfColumns) {
     numberOfColumns = 16;
     numberOfRows = 16;
     for (numberOfRows; numberOfRows>0; numberOfRows--) {
-        let gridNumberOfRows = document.createElement("div");
+        gridNumberOfRows = document.createElement("div");
         gridNumberOfRows.classList.add("row");
         gridNumberOfRows.setAttribute("id",`row-${numberOfRows}`);
-        gridNumberOfRows.style.cssText = "background-color:white; border:2px solid black; height:50px; width:50px";
+        gridNumberOfRows.style.cssText = "border:2px solid black; height:50px; width:50px";
         gridContainer.appendChild(gridNumberOfRows);
         for (numberOfColumns--; numberOfColumns>0; numberOfColumns--){
-            let gridNumberOfColumns = document.createElement("div");
+            gridNumberOfColumns = document.createElement("div");
             gridNumberOfColumns.classList.add("column");
             gridNumberOfColumns.setAttribute("id", `row-${numberOfRows}-column-${numberOfColumns}`);
-            gridNumberOfColumns.style.cssText = "background-color:white; border:2px solid black; height:50px; width:50px"
+            gridNumberOfColumns.style.cssText = "border:2px solid black; height:50px; width:50px"
             gridContainer.appendChild(gridNumberOfColumns);
         }
         numberOfColumns = 16;
     }
 }
-
 createDivs()
+
+const rowDivs = document.querySelectorAll(".row");
+const columnDivs = document.querySelectorAll(".column");
+
+rowDivs.forEach((row) => {
+    row.addEventListener("mouseover", () => {
+        row.classList.add("hovered")
+    })
+})
+
+columnDivs.forEach((column)=>{
+    column.addEventListener("mouseover", () => {
+        column.classList.add("hovered")
+    })
+})
