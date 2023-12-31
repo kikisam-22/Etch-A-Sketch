a2cce123b0d2b5298070d5c9ad32ecf538911df6
kikisam-22 <kiadysam@gmail.com>
2023-12-31 16:49:32 +0100

    Defining funtion for each button. Assigning function to each button in index.html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const circle = document.querySelector(".round");
 const gridContainer = document.querySelector("#grid-container");
 let gridSize = 16;
 
@@ -18,12 +19,6 @@ createDivs(gridSize)
 
 const gridSquares = document.querySelectorAll(".grid-square");
 
-gridSquares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.classList.add("hovered")
-    })
-})
-
 function chooseButton(buttonNumber) {
     const button = document.getElementById("button" + buttonNumber);
 
@@ -32,9 +27,68 @@ function chooseButton(buttonNumber) {
     for (let i = 1; i <= 4; i++){
         const otherButton = document.getElementById("button" + i);
         otherButton.classList.remove("chosen");
+        otherButton.classList.remove("active");
     }
     
     if (!isChosen){
-        button.classList.add("chosen")
+        button.classList.add("chosen");
+        button.classList.add("active");
+    }
+
+}
+
+function randomColor(){
+    const availableColor = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+    let randomIndex = Math.floor(Math.random() * availableColor.length);
+    return availableColor[randomIndex]
+}
+
+function updateSquareColor(color){
+    gridSquares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.cssText=`background-color : ${color} `
+        })
+    })
+}
+
+function activeOriginalMode(){ 
+    if(document.getElementById("button1").classList.contains("active")) {
+        updateSquareColor("black");
+        circle.style.cssText = "background: black";
     }
 }
+
+function activeRainbowMode(){
+    if(document.getElementById("button2").classList.contains("active")) {
+        gridSquares.forEach((square) => {
+            square.addEventListener("mouseover", () => {
+                square.style.cssText=`background-color : ${randomColor()} `
+            })
+        });
+        circle.style.cssText = "background: rgb(255,0,0); background: linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,255,0,1) 50%, rgba(238,130,238,1) 100%);"
+    }
+}
+
+function activeEraseMode(){
+    if(document.getElementById("button3").classList.contains("active")) {
+        updateSquareColor();
+        circle.style.cssText = "background:"
+
+    }
+
+}
+
+function resetAll(){
+    const resetButton = document.querySelector("#button4");
+    const orinalButton = document.querySelector("#button1");
+    gridSquares.forEach((square) => {
+        square.style.cssText="background-color: "
+    });
+    resetButton.classList.remove("active");
+    resetButton.classList.remove("chosen");
+    orinalButton.classList.add("active");
+    orinalButton.classList.add("chosen");
+    activeOriginalMode()    
+}
+
+activeOriginalMode()
